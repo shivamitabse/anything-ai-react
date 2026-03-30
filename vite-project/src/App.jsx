db56bade712e2d481a0004d81ab61be7823aece3
shiva <shivamitabse@gmail.com>
2026-03-30 13:46:51 +0530

    nc

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,15 +2,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Navbar from "./components/Navbar";
+import ProtectedRouteTwo from "./components/ProtectedRouteTwo";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRouteTwo>
+              <Login />
+            </ProtectedRouteTwo>
+          }
+        />
+
+        <Route
+          path="/register"
+          element={
+            <ProtectedRouteTwo>
+              <Register />
+            </ProtectedRouteTwo>
+          }
+        />
 
         <Route
           path="/dashboard"
---
+++ vite-project/src/components/ProtectedRouteTwo.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const [isAuth, setIsAuth] = useState(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/v1/auth/me", {
+          credentials: "include", // 🔥 REQUIRED
+        });
+
+        const data = await res.json();
+
+        if (data.user) {
+          setIsAuth(true);
+        } else {
+          setIsAuth(false);
+        }
+      } catch (err) {
+        setIsAuth(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  // ⏳ while checking
+  if (isAuth === null) {
+    return <p>Loading...</p>;
+  }
+
+  // ✅ authenticated
+  if (isAuth) {
+    return <Navigate to="/dashboard" replace />;
+  }
+
+  // ❌ not authenticated
+  return children;
+}
